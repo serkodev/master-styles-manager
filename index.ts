6b91c519d14840765221229b987f3e23c546ae23
SerKo <serko.dev@gmail.com>
2022-04-23 19:07:43 +0800

    feat: build css properties json

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
+import fs from 'fs';
+import path from 'path';
 import { Styles } from '@master/styles';
-
 import Shorthand from './shorthand';
 import CSSProperties from './css-properties';
 
@@ -18,5 +19,16 @@ if (args[0] == '-c') {
     Styles
         // .filter(style => style.key == 'overflow') // debug
         .forEach(style => { cssProperty.process(style); });
-    console.dir(cssProperty.mapper, {maxArrayLength: null, depth: null});
+
+    if (args[1] == '-o' && args[2]) {
+        const filePath = args[2];
+        const dirname = path.dirname(filePath);
+        if (!fs.existsSync(dirname)) {
+            fs.mkdirSync(dirname);
+        }
+        fs.writeFileSync(args[2], JSON.stringify(cssProperty.mapper));
+        console.log('saved css properties to', filePath);
+    } else {
+        console.dir(cssProperty.mapper, {maxArrayLength: null, depth: null});
+    }
 }
