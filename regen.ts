d130533e47b758fca5d864e47a5e4a8a72579cda
SerKo <serko.dev@gmail.com>
2022-04-24 16:13:37 +0800

    fix: regex support safari

--- regen.ts
+++ regen.ts
@@ -3,8 +3,8 @@ import genex from 'genex';
 export default (regex: RegExp) => {
     let matchesRegex = regex.source;
 
-    // replace wildcard (.) regex for generate
-    matchesRegex = matchesRegex.replace(/(?<!\\)\.[\*\+]?\??/g, ' ');
+    // replace wildcard (.) regex for generate, regex replace is for ignoring \.
+    matchesRegex = matchesRegex.replace(/(\\?)\.[\*\+]?\??/g, (a, b) => !b ? ' ' : a);
 
     return genex(new RegExp(matchesRegex)).generate();
 };
